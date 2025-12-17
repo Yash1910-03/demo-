a5e6d5ebcbde2300446dd546e905c80611b08fcb
Yash1910-10 <joshiyash256@gmail.com>
2025-12-17 11:32:04 +0530

    concept.txt

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 
-from math import pow
-from math import sqrt
-from math import cbrt
+# from math import pow
+# from math import sqrt
+# from math import cbrt
 
-number=int(input("enter the number"))
-print(f"value is:{pow(number,5)}")
-print(f"value is:{sqrt(number)}")
-print(f"value is:{cbrt(number)}")
+# number=int(input("enter the number"))
+# print(f"value is:{pow(number,5)}")
+# print(f"value is:{sqrt(number)}")
+# print(f"value is:{cbrt(number)}")
 
-password=input("enter your password")
-if (len(password)<=8):
-    print(f'apke account se paisa gayab ho sakta he')
-else:
-    print('will check and revert')
+# password=input("enter your password")
+# if (len(password)<=8):
+#     print(f'apke account se paisa gayab ho sakta he')
+# else:
+#     print('will check and revert')
 
 password_data=input("enter number")
 if(password_data.isalnum()):
     print("pass has both num and alphabets")
 else:
-    print("others")
+    print("others are not alpha numeric")
+
+
     
